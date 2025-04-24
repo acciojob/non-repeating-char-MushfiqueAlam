9ce75e6bd92a3adb4685a72077cfbb1e2b527076
Mushfique Alam <162487127+MushfiqueAlam@users.noreply.github.com>
2025-04-25 01:29:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ function firstNonRepeatedChar(str) {
  // Write your code here
 	const charArray={};
 	for (let ch of str) {
-		charArray[ch]=(charArray[char] || 0) + 1;
+		charArray[ch]=(charArray[ch] || 0) + 1;
 		
 	}
-	 for (let char of str) {
-        if (charCount[char] === 1) {
-            return char;
+	 for (let ch of str) {
+        if (charArray[ch] === 1) {
+            return ch;
         }
     }
 
 	return null;
-	
+
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
